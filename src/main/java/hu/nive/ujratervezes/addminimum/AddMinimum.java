d87c2782fa9bdbc97f969ec2d021a5acbc24e111
Sziku <105989158+Sziku@users.noreply.github.com>
2022-12-05 12:11:46 +0100

    add addMinimum method and write a possible solution and make the exceptions

--- src/main/java/hu/nive/ujratervezes/addminimum/AddMinimum.java
+++ src/main/java/hu/nive/ujratervezes/addminimum/AddMinimum.java
@@ -1,5 +1,25 @@
 package hu.nive.ujratervezes.addminimum;
 
 public class AddMinimum {
+    public int[] addMinimum(int[] numbers, int n)  {
+        if(numbers.length == 0){
+            return new int[0];
+        } else if (numbers == null || n < 1) {
+            try {
+                throw new  IllegalAccessException();
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
+        int minumum = numbers[0];
+
+        for(int number : numbers){
+            if(number < minumum){
+                minumum = number;
+            }
+        }
+
+
+    }
 }
