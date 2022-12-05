eae267133354db98341e64843cda3a6df4142a09
Sziku <105989158+Sziku@users.noreply.github.com>
2022-12-05 12:31:12 +0100

    finished task and all test passed

--- src/main/java/hu/nive/ujratervezes/addminimum/AddMinimum.java
+++ src/main/java/hu/nive/ujratervezes/addminimum/AddMinimum.java
@@ -1,15 +1,15 @@
 package hu.nive.ujratervezes.addminimum;
 
+import java.util.Arrays;
+
 public class AddMinimum {
     public int[] addMinimum(int[] numbers, int n)  {
-        if(numbers.length == 0){
+        if(numbers == null){
+            throw new IllegalArgumentException();
+        } else if(numbers.length == 0){
             return new int[0];
-        } else if (numbers == null || n < 1) {
-            try {
-                throw new  IllegalAccessException();
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
+        } else if ( n < 1) {
+            throw new IllegalArgumentException();
         }
 
         int minumum = numbers[0];
@@ -19,7 +19,11 @@ public class AddMinimum {
                 minumum = number;
             }
         }
-
-
+        for(int i = n - 1; i< numbers.length;){
+            int tmp = numbers[i];
+            numbers[i] = tmp+minumum;
+            i = i + n;
+        }
+        return numbers;
     }
 }
